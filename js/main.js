cd1f585412f049563e89e2e531231284dbca2a84
A-Srinivasa-reddy <35569925+A-Srinivasa-reddy@users.noreply.github.com>
2022-05-31 11:25:01 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -44,3 +44,34 @@ $(function () {
     },
   });
 });
+function getLocation() {
+  function success(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    alert(latitude)
+    document.getElementById("latitude").value = String(latitude);
+    document.getElementById("longitude").value = String(longitude);
+  }
+  function showError(error) {
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        document.getElementById("latitude").value="User denied the request for Geolocation.";
+        break;
+      case error.POSITION_UNAVAILABLE:
+        document.getElementById("latitude").value="Location information is unavailable.";
+        break;
+      case error.TIMEOUT:
+        document.getElementById("latitude").value="The request to get user location timed out.";
+        break;
+      case error.UNKNOWN_ERROR:
+        document.getElementById("latitude").value="An unknown error occurred.";
+        break;
+    }
+  }
+  if (!navigator.geolocation) {
+    document.getElementById("latitude").value="Cannot Update"
+    return(["Cannot Update", "Cannot Update"]);
+  } else {
+    navigator.geolocation.getCurrentPosition(success, showError);
+  }
+}
