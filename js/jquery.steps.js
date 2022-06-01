015f227116fdbfd0f7c54f966df2e6b40bc18a32
A-Srinivasa-reddy <35569925+A-Srinivasa-reddy@users.noreply.github.com>
2022-06-01 10:23:07 +0530

    console

--- js/jquery.steps.js
+++ js/jquery.steps.js
@@ -2029,6 +2029,7 @@
             fetch(url)
               .then((r) => r.json())
               .then(function (data) {
+                console.log(data);
                 var x = document.getElementById("branddetails");
                 for (var i = 0; i < data.length; i++) {
                   var div = document.createElement("div");
